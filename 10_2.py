338f569c96365d83ee4c3252311345116d699209
lika bazghadze <likabazghadze@likas-MacBook-Pro.local>
2024-10-27 19:50:15 +0400

    10_2.py

---
+++ 10_2.py
@@ -0,0 +1,7 @@
+def find_maximum(*numbers):
+    return max(numbers)
+
+print(find_maximum(10, 5, 3, 12, 7))
+print(find_maximum(4.5, 6.3, 1.2, 9.8))
+print(find_maximum(100, 200, 300)) 
+print(find_maximum(42))    
