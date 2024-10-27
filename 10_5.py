c4a580a5dc007f49cb672d6c04800c03621ab0a8
lika bazghadze <likabazghadze@likas-MacBook-Pro.local>
2024-10-27 19:43:21 +0400

    10_5.py

---
+++ 10_5.py
@@ -0,0 +1,10 @@
+def reverse_text(text):
+    return text[::-1]
+    
+text1 = input("enter text: ")
+text2 = input("enter text: ")
+text3 = input("enter text: ")
+
+print(reverse_text(text1))
+print(reverse_text(text2))
+print(reverse_text(text3))
