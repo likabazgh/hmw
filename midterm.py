388b67de7774821e75c2dda9917cea76cf45d2a3
lika bazghadze <likabazghadze@likas-MacBook-Pro.local>
2024-11-11 20:59:31 +0400

    midterm.py

---
+++ midterm.py
@@ -0,0 +1,8 @@
+input_name = input("enter name: ")
+input_lastname = input("enter lastname: ")
+input_age = input("enter age: ")
+input_city = input("enter city: ")
+
+print(f"hello {input_name} {input_lastname}")
+print(f"age: {input_age}")
+print(f"city: {input_city}")
